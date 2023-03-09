ed44ed2ce042cff26e9312e35e7b3ff214c9dcab
maike <maike@Maikelpc>
2023-03-09 10:12:46 +0100

    holaaa

---
+++ src/main/java/com/miltxonborda/marzo/NOMBRE.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.miltxonborda.marzo;
+
+/**
+ *
+ * @author maike
+ */
+public class NOMBRE {
+    public static void main(String[] args) {
+        System.out.println("estamos en la rama EXAMEN");
+        System.out.println("maikel mendoza");
+    }
+}
